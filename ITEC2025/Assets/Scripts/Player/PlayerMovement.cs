8f53401487dbb2c6c1262af6f36a80c5ff428c87
goodKDSmAAdcity <128930159+goodKDSmAAdcity@users.noreply.github.com>
2025-04-05 17:16:11 +0300

    l1

--- ITEC2025/Assets/Scripts/Player/PlayerMovement.cs
+++ ITEC2025/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement;
     public Animator anim;
 
+    [Header("Custom Keybinds")]
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,20 +20,32 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement = Vector2.zero;
+
+        if (Input.GetKey(upKey))
+            movement.y += 1;
+        if (Input.GetKey(downKey))
+            movement.y -= 1;
+        if (Input.GetKey(leftKey))
+            movement.x -= 1;
+        if (Input.GetKey(rightKey))
+            movement.x += 1;
+
         anim.SetBool("isMoving", IsMoving());
-        if (IsMoving() ){
+
+        if (IsMoving())
+        {
             anim.SetFloat("Horizontal", movement.x);
             anim.SetFloat("Vertical", movement.y);
         }
+
         movement.Normalize();
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-    } 
+    }
 
     bool IsMoving()
     {
