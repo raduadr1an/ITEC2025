65927e0311d876bbe2261d7f20d20fc3f63f3305
Radu <radu.a.mocan@gmail.com>
2025-04-05 19:19:35 +0300

    order

--- ITEC2025/Assets/Scripts/Display/TMPDisplay.cs
+++ ITEC2025/Assets/Scripts/Display/TMPDisplay.cs
@@ -3,54 +3,66 @@ using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TMPDisplay: MonoBehaviour
+public class TMPDisplay : MonoBehaviour
 {
     private TextMeshProUGUI tmpText;
-    private List<string> textSequences;
+    private List<string> textSequences = new List<string>
+    {
+        "First line of text.",
+        "Second line of text.",
+        "Third line of text."
+    };
     public float charactersPerSecond = 10f;
-    public float delayBetweenTexts = 1f;
 
     private int currentTextIndex = 0;
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
 
     void Start()
     {
         tmpText = GetComponent<TextMeshProUGUI>();
-        textSequences = new List<string>
-        {
-            "First line of text.",
-            "Second line of text.",
-            "Third line of text."
-        };
 
-        if (tmpText == null)
-            tmpText = GetComponent<TextMeshProUGUI>();
-
-        if (textSequences.Count > 0)
-            StartCoroutine(DisplayTextsSequentially());
+        DisplayNextText();
     }
 
-    IEnumerator DisplayTextsSequentially()
+    void Update()
     {
-        while (currentTextIndex < textSequences.Count)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            yield return StartCoroutine(TypeText(textSequences[currentTextIndex]));
-            yield return new WaitForSeconds(delayBetweenTexts);
+            if (isTyping)
+            {
+                StopCoroutine(typingCoroutine);
+                tmpText.maxVisibleCharacters = tmpText.text.Length;
+                isTyping = false;
+            }
+            else
+            {
+                DisplayNextText();
+            }
+        }
+    }
+
+    void DisplayNextText()
+    {
+        if (currentTextIndex < textSequences.Count)
+        {
+            typingCoroutine = StartCoroutine(TypeText(textSequences[currentTextIndex]));
             currentTextIndex++;
         }
     }
 
     IEnumerator TypeText(string text)
     {
+        isTyping = true;
         tmpText.text = text;
         tmpText.maxVisibleCharacters = 0;
-        int totalVisibleCharacters = text.Length;
-        int counter = 0;
 
-        while (counter < totalVisibleCharacters)
+        foreach (char c in text)
         {
-            counter++;
-            tmpText.maxVisibleCharacters = counter;
+            tmpText.maxVisibleCharacters++;
             yield return new WaitForSeconds(1f / charactersPerSecond);
         }
+
+        isTyping = false;
     }
 }
