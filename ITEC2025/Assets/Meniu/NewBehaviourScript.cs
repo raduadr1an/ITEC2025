c2a363cf63ba608f32821de37ba063032b7bfaf3
SebiTheyThem <166058726+SebiTheyThem@users.noreply.github.com>
2025-04-05 11:14:06 +0300

    FUNCTIONEAZA

--- ITEC2025/Assets/Meniu/NewBehaviourScript.cs
+++ ITEC2025/Assets/Meniu/NewBehaviourScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SC_MainMenu : MonoBehaviour
 {
     public GameObject MainMenu;
-    public GameObject CreditsMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +18,10 @@ public class SC_MainMenu : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
     }
 
-    public void CreditsButton()
-    {
-        // Show Credits Menu
-        MainMenu.SetActive(false);
-        CreditsMenu.SetActive(true);
-    }
-
     public void MainMenuButton()
     {
         // Show Main Menu
         MainMenu.SetActive(true);
-        CreditsMenu.SetActive(false);
     }
 
     public void QuitButton()
